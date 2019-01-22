c6d1b6291328c8b64e5349e0aa7ac298ae693760
tapps <t@thisroad.org>
2019-01-21 19:07:34 -0600

    scroll to height adjust, temp disable mobile menu

--- assets/js/scripts/scrollto-helper.js
+++ assets/js/scripts/scrollto-helper.js
@@ -11,7 +11,7 @@ jQuery(document).ready( function($) {
 				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 				if (target.length) {
 					$('html,body').animate({
-						scrollTop: target.offset().top-150
+						scrollTop: target.offset().top-50
 					}, 1000);
 					return false;
 				}
