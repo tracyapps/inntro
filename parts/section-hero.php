9a9bef58cdd100cc8c964bd7f556cdc7ce89c3e0
tapps <t@thisroad.org>
2019-02-09 18:01:58 -0600

    more a11y friendly colors and gradients, and switch hero text to left

--- parts/section-hero.php
+++ parts/section-hero.php
@@ -16,9 +16,13 @@ $image  = get_sub_field( 'hero_image' );
 
 		<?php the_sub_field( 'hero_text' ); ?>
 
+		<?php if( get_sub_field( 'hero_button_label' ) ) { ?>
+
 		<a class="button primary" href="<?php echo esc_url( get_sub_field( 'hero_button_link' ) ); ?>">
 			<?php echo esc_html( get_sub_field( 'hero_button_label' ) ); ?>
 		</a>
+
+		<?php } else {} ?>
 
 	</div>
 
