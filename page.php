e8af7e3536ea651fa22314b9c90d2551029eada5
tapps <t@thisroad.org>
2020-06-24 01:59:55 -0500

    page template fixes and first section top padding

--- page.php
+++ page.php
@@ -2,27 +2,23 @@
 
 <div id="content">
 
-	<div id="inner-content" class="row">
+	<main id="main" class="full" role="main">
 
-		<main id="main" class="full" role="main">
-
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 
-				<?php
-				if( get_option( 'options_disable_sections_on_pages' ) == 1 ) :
-					echo '<div class="container">';
-					get_template_part( 'parts/loop', 'page' );
-					echo '</div>';
-				else :
-					get_template_part( 'parts/sections' );
-				endif; ?>
+			<?php
+			if( get_option( 'options_disable_sections_on_pages' ) == 1 ) :
+				echo '<div class="container">';
+				get_template_part( 'parts/loop', 'page' );
+				echo '</div>';
+			else :
+				get_template_part( 'parts/sections' );
+			endif; ?>
 
-			<?php endwhile; endif; ?>
+		<?php endwhile; endif; ?>
 
-		</main> <!-- end #main -->
-
-	</div> <!-- end #inner-content -->
+	</main> <!-- end #main -->
 
 </div> <!-- end #content -->
 
